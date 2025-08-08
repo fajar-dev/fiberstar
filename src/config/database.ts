ba92ef586ca2322454f137c9fa0ce0c59bcfc3b7
fajar-dev <fajarrivaldi2015@gmail.com>
2025-08-08 15:43:21 +0700

    init: postgree

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,12 +5,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const dbConfig = new DataSource({
-    type: "mysql",
-    host: process.env.MYSQL_HOST,
-    port: Number(process.env.MYSQL_PORT),
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: false,
     logging: false,
     entities: [HomePass],
